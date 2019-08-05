abd97cc8dffa36887bf773479eaaef145edf5ea6
Pankaj <38392993+Pankaj01998@users.noreply.github.com>
2019-08-05 13:08:44 +0530

    Update model.py

--- genomita/DeepSEA.md/model.py
+++ genomita/DeepSEA.md/model.py
@@ -37,31 +37,5 @@ class net (nn.Module):
 
 
 model = net()
-# print(model)
-# print (list(net.parameters())[0])  
-
-# Initialize optimizer
-optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-
-# Print model's state_dict
-print("Model's state_dict:")
-for param_tensor in model.state_dict():
-    print(param_tensor, "\t", model.state_dict()[param_tensor].size())
-
-# Print optimizer's state_dict
-print("Optimizer's state_dict:")
-for var_name in optimizer.state_dict():
-    print(var_name, "\t", optimizer.state_dict()[var_name])
-
-# Load the Drive helper and mount
-from google.colab import drive
-
-# This will prompt for authorization.
-drive.mount('/content/drive')
 
 print (model)
-
-input = torch.randn(1, 1, 1000, 4)
-out = model(input)
-
-out.shape
