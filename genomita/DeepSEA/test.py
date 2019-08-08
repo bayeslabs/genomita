ae447f3b1e85fe3131526e5a1019c45eeed3840d
Pankaj <38392993+Pankaj01998@users.noreply.github.com>
2019-08-08 09:29:19 +0530

    Update test.py

--- genomita/DeepSEA/test.py
+++ genomita/DeepSEA/test.py
@@ -2,45 +2,16 @@ import torch
 import math
 import torch.nn as nn
 import torch.nn.functional as F
+import model
 
-class net (nn.Module):
-  def __init__(self):
-    super(net, self).__init__()
-    self.conv1 = nn.Conv2d(1, 320 , (8, 4))
-    self.dp1 = nn.Dropout(p = 0.2)
-    self.conv2 = nn.Conv2d(320, 480, (8, 1))
-    self.dp2 = nn.Dropout(p = 0.2)
-    self.conv3 = nn.Conv2d(480, 960, (8, 1))
-    self.dp3 = nn.Dropout(p = 0.5)
-    width  = 1000
-    nchannel = math.floor((math.floor((width-7)/4.0)-7)/4.0)-7
-    print (nchannel)
-    self.fc1 = nn.Linear( 960*nchannel, 919)
-   
-  def forward(self, x):
-    x = self.conv1(x)
-    x = F.max_pool2d(x, (4, 1), stride=4)
-    x = self.dp1(x)
-    x = self.conv2(x)
-    x = F.max_pool2d(x, (4, 1), stride=4)
-    x = self.dp2(x)
-    x = self.conv3(x)
-    x = self.dp3(x)
-    x = torch.flatten(x)
-    x = self.fc1(x)
-    x = torch.sigmoid(x)
-    return x
-
-# Load the Drive helper and mount
-from google.colab import drive
-
-# This will prompt for authorization.
-drive.mount('/content/drive')
+#if you want to import from google colab folder you can use this below commented code 
+# from google.colab import drive
+# drive.mount('/content/drive')
 
 model = net()
-model.load_state_dict(torch.load('/content/drive/My Drive/colab/dataset/trained_model.pth'))
+model.load_state_dict(torch.load('/data/trained_model.pth'))
 
-seqs = torch.load('/content/drive/My Drive/colab/dataset/seqs_9.pt')
+seqs = torch.load('/data/seqs_9.pt')
 
 model.eval()
 for i in range(seqs.shape[0]):
